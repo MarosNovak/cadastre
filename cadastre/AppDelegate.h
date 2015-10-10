e7e7002230dd3578d94def3a52e57839195efe0e
marosnovak <novkmaros@gmail.com>
2015-10-10 18:00:36 +0200

    First commit

---
+++ cadastre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cadastre
+//
+//  Created by Maros Novák on 10/10/15.
+//  Copyright © 2015 Maros Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cadastre/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  cadastre
+//
+//  Created by Maros Novák on 10/10/15.
+//  Copyright © 2015 Maros Novak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
