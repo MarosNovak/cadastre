22633b6f19168c177f928fbf5a24c9e134b16298
marosnovak <novkmaros@gmail.com>
2015-10-22 02:13:40 +0200

    Area detail controller

---
+++ cadastre/CadastreAreaDetailController.h
@@ -0,0 +1,13 @@
+//
+//  CadastreAreaDetailControllerTableViewController.h
+//  cadastre
+//
+//  Created by Maros Novák on 22/10/15.
+//  Copyright © 2015 Maros Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CadastreAreaDetailController : UITableViewController
+
+@end
